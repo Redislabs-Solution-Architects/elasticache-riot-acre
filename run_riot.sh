f1d12ec2cf0f2df9b9b39750fb7b1d791073b99c
DaShaun <dashaun@dashaun.com>
2021-11-13 08:07:17 -0600

    cleanup

--- run_riot.sh
+++ run_riot.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-rm privateKey
-
-#az login --service-principal --username "$TF_VAR_client_id" --password "$TF_VAR_client_secret" --tenant "$TF_VAR_tenant_id"
-#az keyvault secret show --name redisgeekr2dwzktholjk6 --vault-name redisgeekr2dwzktholjk6
+rm -rf privateKey
 
 export EC_HOST=$(terraform output -raw ec | jq -r .address)
 export EC_PORT=$(terraform output -raw ec | jq -r .port)
@@ -12,13 +9,12 @@ export RIOT_HOST=$(terraform output -raw riot | jq -r .host)
 export RIOT_BIN=$(terraform output -raw riot | jq -r .program)
 
 export ACRE_HOST=$(terraform output -raw acre | jq -r .host)
-export ACRE_REGION=$(terraform output -raw acre | jq -r .region)
 export ACRE_PORT=$(terraform output -raw acre | jq -r .port)
-export ACRE_FQDN="$ACRE_HOST.$ACRE_REGION.redisenterprise.cache.azure.net"
-export ACRE_PASS="5SJChCSOkcql18etjT2Jkna3AwIQmaUdXY6QHwoHhg0="
+export ACRE_PASS=$(terraform output -raw acre | jq -r .access_key)
 
 echo "Executing RIOT via SSH"
 touch privateKey
 chmod 600 privateKey
 terraform output -raw sensitive | jq -r .private_key_pem >> privateKey
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i privateKey "$RIOT_USER"@"$RIOT_HOST" "$RIOT_BIN -h $EC_HOST -p $EC_PORT  replicate -h $ACRE_FQDN -p $ACRE_PORT --idle-timeout 10000 --live --tls --no-verify-peer -a $ACRE_PASS"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i privateKey "$RIOT_USER"@"$RIOT_HOST" "$RIOT_BIN -h $EC_HOST -p $EC_PORT  replicate -h $ACRE_HOST -p $ACRE_PORT --idle-timeout 10000 --mode live --tls --no-verify -a $ACRE_PASS"
+rm -rf privateKey
