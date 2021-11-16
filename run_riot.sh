b7f7c09b15aea700b8f6d9cc4b8b05cf2ec9ce82
DaShaun <dashaun@dashaun.com>
2021-11-15 23:43:35 -0600

    quotes

--- run_riot.sh
+++ run_riot.sh
@@ -3,16 +3,16 @@ rm -rf privateKey
 
 export IDLE_TIMEOUT=10000
 
-export EC_HOST=$(terraform output -raw ec | jq -r .address)
-export EC_PORT=$(terraform output -raw ec | jq -r .port)
+export EC_HOST=$(terraform output -raw ec | jq -r '.address')
+export EC_PORT=$(terraform output -raw ec | jq -r '.port')
 
-export RIOT_USER=$(terraform output -raw riot | jq -r .user)
-export RIOT_HOST=$(terraform output -raw riot | jq -r .host)
-export RIOT_BIN=$(terraform output -raw riot | jq -r .program)
+export RIOT_USER=$(terraform output -raw riot | jq -r '.user')
+export RIOT_HOST=$(terraform output -raw riot | jq -r '.host')
+export RIOT_BIN=$(terraform output -raw riot | jq -r '.program')
 
-export ACRE_HOST=$(terraform output -raw acre | jq -r .host)
-export ACRE_PORT=$(terraform output -raw acre | jq -r .port)
-export ACRE_PASS=$(terraform output -raw acre | jq -r .access_key)
+export ACRE_HOST=$(terraform output -raw acre | jq -r '.host')
+export ACRE_PORT=$(terraform output -raw acre | jq -r '.port')
+export ACRE_PASS=$(terraform output -raw acre | jq -r '.access_key')
 
 echo "Executing RIOT via SSH"
 touch privateKey
